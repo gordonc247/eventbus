0ada257fdcff132e3459eb204dd7e92842827983
Gordon Craig <gordon.craig@pipware.net>
2021-04-19 17:14:33 +1000

    Add in-memory implmentation for IEventBus and ISubscriptionManager

---
+++ src/Memory/MemoryEventBus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Codefire.EventBus.Memory
+{
+    public class MemoryEventBus : IEventBus
+    {
+        public MemoryEventBus(ISubscriptionManager subscriptionManager, IServiceProvider serviceProvider, ILogger<MemoryEventBus> logger)
+        {
+            SubscriptionManager = subscriptionManager;
+            ServiceProvider = serviceProvider;
+            Logger = logger;
+        }
+
+        private ISubscriptionManager SubscriptionManager { get; }
+        private IServiceProvider ServiceProvider { get; }
+        private ILogger Logger { get; }
+
+        public async Task PublishAsync<TEvent>(TEvent evt)
+            where TEvent : IntegrationEvent
+        {
+            var handlers = SubscriptionManager.GetHandlers<TEvent>();
+
+            foreach (var handler in handlers)
+            {
+                var instance = ServiceProvider.GetService(handler);
+                if (instance is IIntegrationEventHandler<TEvent> subscriber)
+                {
+                    try
+                    {
+                        await subscriber.HandleAsync(evt);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.LogError(new EventId(), ex, "EXCEPTION");
+                    }
+                }
+            }
+        }
+
+        public SubscriptionToken Subscribe<TEvent, THandler>()
+            where TEvent : IntegrationEvent
+            where THandler : IIntegrationEventHandler<TEvent>
+        {
+            return SubscriptionManager.AddSubscription<TEvent, THandler>();
+        }
+
+        public void Unsubscribe(SubscriptionToken token)
+        {
+            SubscriptionManager.RemoveSubscription(token);
+        }
+    }
+}
---
+++ src/SubscriptionManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codefire.EventBus
+{
+    public class SubscriptionManager : ISubscriptionManager
+    {
+        private static readonly object SubscriptionsLock = new object();
+        private readonly Dictionary<Type, List<SubscriptionInfo>> _subscriptions;
+
+        public SubscriptionManager()
+        {
+            _subscriptions = new Dictionary<Type, List<SubscriptionInfo>>();
+        }
+
+        public SubscriptionToken AddSubscription<TEvent, THandler>()
+            where TEvent : IntegrationEvent
+            where THandler : IIntegrationEventHandler<TEvent>
+        {
+            lock (SubscriptionsLock)
+            {
+                var eventType = typeof(TEvent);
+                var handlerType = typeof(THandler);
+
+                if (!_subscriptions.TryGetValue(eventType, out var allSubscriptions))
+                {
+                    allSubscriptions = new List<SubscriptionInfo>();
+                    _subscriptions.Add(eventType, allSubscriptions);
+                }
+
+                var token = new SubscriptionToken(typeof(TEvent));
+                allSubscriptions.Add(new SubscriptionInfo(token, handlerType));
+
+                return token;
+            }
+        }
+
+        public void RemoveSubscription(SubscriptionToken token)
+        {
+            if (token == null)
+                throw new ArgumentNullException(nameof(token));
+
+            lock (SubscriptionsLock)
+            {
+                if (!_subscriptions.TryGetValue(token.EventType, out var allSubscriptions)) return;
+
+                allSubscriptions.RemoveAll(x => x.Token.Id == token.Id);
+            }
+        }
+
+        public IEnumerable<Type> GetHandlers<TEvent>() where TEvent : IntegrationEvent
+        {
+            var eventType = typeof(TEvent);
+            lock (SubscriptionsLock)
+            {
+                return _subscriptions.TryGetValue(eventType, out var allSubscriptions)
+                    ? allSubscriptions.Select(x => x.Handler)
+                    : Enumerable.Empty<Type>();
+            }
+        }
+    }
+}
